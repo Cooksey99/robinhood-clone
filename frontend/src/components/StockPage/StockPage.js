0f4a1530a91b72be501812f26c4414519f20ec45
cooksey <andrewcooksey42@gmail.com>
2022-03-22 16:36:31 -0500

    stock chart working

--- frontend/src/components/StockPage/StockPage.js
+++ frontend/src/components/StockPage/StockPage.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
-import { Chart } from 'react-chartjs-2';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
 import { getAsset } from '../../store/asset';
 import { finnhubFetch, finnhubClient} from '../finnhubSetup';
+import { StockChart } from '../Portfolio/StockChart';
 import { StockSideBar } from '../Sidebar/StockSidebar';
 import './stockPage.css'
 
@@ -13,12 +13,11 @@ export const StockPage = () => {
     const { symbol } = useParams();
     const dispatch = useDispatch();
     const stock = useSelector(state => state.assetReducer);
-    const [stockInfo, setStockInfo] = useState({})
+    const [stockInfo, setStockInfo] = useState({});
 
 
     useEffect(() => {
         dispatch(getAsset(symbol));
-        finnhubFetch()
 
         finnhubClient.quote(symbol, (error, data, response) => {
             // console.log(data)
@@ -33,6 +32,7 @@ export const StockPage = () => {
                 <h1>{symbol}</h1>
                 <h3>daily change: {stockInfo.dp}</h3>
                 <h3>current: {stockInfo.c}</h3>
+                <StockChart option='asset'/>
                 <h3>high: {stockInfo.h}</h3>
                 <h3>low: {stockInfo.l}</h3>
             </div>
--- frontend/src/components/finnhubSetup.js
+++ frontend/src/components/finnhubSetup.js
@@ -9,35 +9,3 @@ export const finnhubClient = new finnhub.DefaultApi()
 
 const BASE_URL = 'finnhub.io/api/v1';
 const API_KEY = 'c8obubqad3iddfsarfeg';
-
-let stockInfo = {};
-
-export const finnhubFetch = () => {
-    // finnhubClient[reqType](stock, (error, data, response) => {
-    //   console.log(data)
-    // })
-
-    finnhubClient.quote("AAPL", (error, data, response) => {
-        // console.log(data)
-        stockInfo = data;
-    });
-    console.log('stockInfo', stockInfo);
-    // fetch(`https://finnhub.io/api/v1/quote?symbol=AAPL&token=c8obubqad3iddfsarfeg`)
-
-    // let final = await testing.json();
-    // console.log('second time', testing)
-    // return result;
-    // let result = await axios.get('https://finnhub.io/api/v1/quote?symbol=AAPL&token=c8obubqad3iddfsarfeg')
-    //     .then(function (response) {
-    //         console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //         console.log(error);
-    //     })
-    //     .then(function (){
-    //     });
-
-    // console.log('result', result)
-
-}
-
